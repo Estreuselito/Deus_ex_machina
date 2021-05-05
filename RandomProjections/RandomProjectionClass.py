f23ac08c2ee280fb189809942092a8a474d28a24
Yannik Suhre <yannik@familie-suhre.de>
2021-05-05 22:00:26 +0200

    current changes

--- RandomProjections/RandomProjectionClass.py
+++ RandomProjections/RandomProjectionClass.py
@@ -20,16 +20,11 @@ class RandomSparseRepresentation:
             self._printmd("---")
         if text:
             self._printmd(
-"""Welcome to the interface of **RandomSparseRepresentation**! :)
+"""
+# Welcome to the interface of **RandomSparseRepresentation**!
         
 You have now instantiated an object, with which you can create a RandomSparseRepresentation.
-In order to do so, please first pick a dataset from this website [UCI ML](https://archive.ics.uci.edu/ml/index.php).
-
-Once you have done so, please use the function ```get_data()``` on your object to download that data. 
-This function takes one necessary parameter and an optional one. The necessary one is the URL to
-the dataset you obtain when you right click in the data folder on the dataset and copy that link.
-Should the dataset not be a `.csv` within the datafolder on the UCI website, but rather a `.data`
-please also provide the column names as a list, which you can find in the `.names` file in the datafolder.""")
+The function ```get_data()``` retrieves the data, which was previously downloaded from the sources outlined above.""")
         
     def get_data(self, url: str, data_type: str = None, names: list = None, text: bool = True, **kwargs):
         try:
@@ -41,13 +36,13 @@ please also provide the column names as a list, which you can find in the `.name
             elif url and data_type in [".csv", ".data"]:
                 self.data = pd.read_csv(url)
             else:
-                raise AttributeError(f"""The chosen you `data_type = {data_type}` is currently not supported
+                raise AttributeError(f"""The chosen `data_type = {data_type}` is currently not supported
                                      by this function. All supported `data_types` are `.csv`, `.data`, 
                                      `.xls`, `.xlsx` and everything else which can be read with `pd.read_csv
                                      or `pd.read_excel`.""")
             if text:
                 self._printmd(
-"""You successfully downloaded your dataset to the object!
+"""You successfully loaded your dataset to the object!
 
 Now we can go ahead and split the data.
 Please call the `split_data()` function for it. You can pass it the `test_size` parameter, to split your
@@ -64,8 +59,10 @@ Hence they are all printed out in the next step.
 
 {[x for x in self.data.columns]}
 
-In the next step please input a column name, which is contains your target variable.""")
+In the next step please input a column name, which contains your target variable.""")
             time.sleep(1)
+        elif not text:
+            self._printmd(f"The columns of this dataset are: {[x for x in self.data.columns]}")
         while True:
             target = input(prompt = "Please input your target variable here: ")
             if target not in self.data.columns:
@@ -87,25 +84,23 @@ In the next step please input a column name, which is contains your target varia
             
         if text:
             self._printmd(
-f"""Your data has now be splitted into a train and test set by a ratio of `{test_size}`.
-This was done, by selecting the column `{target}` as target column and the rest as independent variables.""")
+f"""Your data has now be split into a train and test set by a factor of `{test_size}`.
+This was done by selecting the column `{target}` as the target column and the rest as independent variables.""")
     
-    def JL_lemma(self, epsilon=0.1):
-        """Sebastian & Skyler will write something about the JL lemma, why it works with small datasets."""
-        
+    def JL_lemma(self, epsilon=0.1):        
         self._printmd(
 f"""In general, the theory of Professor Johnson and Professor Lindenstrauss posits
 the amount of columns to which we can reduce our dataset without losing any distance related information.
 We can specify a parameter called `epsilon` which determines the margin in which the distance is contained.
 
-Our current dataset has {self.data.shape[0]} observations. Using the JL algorithm, we could reduce it to
+Our current dataset has {self.data.shape[0]} observations and {self.data.shape[1]} dimensions. Using the JL algorithm $k$ = 
 {johnson_lindenstrauss_min_dim(self.data.shape[0], eps = epsilon)} dimensions.""")
         if johnson_lindenstrauss_min_dim(self.data.shape[1], eps = epsilon) > self.data.shape[1]:
             self._printmd(
-"""The JL also works, if we have a smaller dataset... **Ask group**!""")
+"""Nevertheless, dimensionality reduction can still work with lower dimensions.""")
         self._printmd(
-"""The next step is to set a define a baseline metric, on which we want to evaluate
-our algorithm with our later reduced dataset. For this please call the function `baseline()`.""")
+"""The next step is to set a baseline metric, which can be used to evaluate the algorithm with
+the reduced dataset. For this please call the function `baseline()`.""")
         
     def baseline(self, model = None, text: bool = True, **kwargs):
         if not model:
@@ -118,7 +113,7 @@ our algorithm with our later reduced dataset. For this please call the function 
             raise e
         if text:
             self._printmd(
-r"""In order to asses the performance of a classifier, it is important to incorporate a numerical evaluation of the algorithm. 
+r"""In order to assess the performance of a classifier, it is important to incorporate a numerical evaluation of the algorithm. 
 For this, a variety of performance measures are available. It is essential to make use of an adequate performance measure as 
 their applicability and significance depend on the dataset as well as the specific classification task.
 There are a few metrics we can choose from, the needed API (which you need to input next) can be viewed
@@ -182,7 +177,7 @@ like `accuracy_score` if you want to use `accuracy_score` or respective for all 
                 self._printmd("""You have selected the `f1_score`. Since the `f1_score` can have different
                 usages as described earlier, depending on its inputs please input some keyword-arguments into
                 the next prompt.
-                Please be advised, that this is done via `average="binary", ...`, meaning with a = as separator.""")
+                Please be advised, that this is done via `average="weighted", ...`, meaning with a = as separator.""")
                 while True:
                     self.kwargs = input(prompt = "Please enter your keyword arguments here: ")
                     try:
@@ -212,51 +207,38 @@ It uses a random matrix to perform the projection and hence the name random proj
 This matrix is also sometimes refered to as map.
 
 If the original dimension of data is $d$ and the target or projected dimension is $k$, where $k<<d$ 
-then the random matrix is of size $k X d$. The random projection can be explained as below
+then the random matrix is of size $k,\ d$. The random projection is explained below.
 
-$X_{kXN}^{RP}$ = $R_{kXd}$$X_{dXN}$
+$X_{k,\ N}^{RP}$ = $R_{k,\ d}$$X_{d,\ N}$
 
 Where
 
-$X_{kXN}^{RP}$ is the random projected N observations in k dimension.
+$X_{k,\ N}^{RP}$ is the random projected N observations in $k$ dimensions,
 
-$R_{kXd}$ is the random matrix used for the projection or transformation.
+$R_{k,\ d}$ is the random matrix used for the projection or transformation,
 
-$X_{dXN}$ is the original N observations in d-dimension.
+$X_{d,\ N}$ is the original $N$ observations in d-dimension.
 
-There are few techniques to create the random matrix. Gaussian and Sparse are just 2 among them.
+There are a few techniques to create the random matrix. Gaussian and Sparse are just 2 among them.
 
 **Gaussian** – The random matrix is created in such a way that each entry is independently drawn from
-the standard normal distribution $N(0, \frac{1}{n_{components}})$. Where $n_{components} is the number of target dimension. 
+the standard normal distribution $N(0, \frac{1}{n_{components}})$. Where $n_{components}$ is the dimensionality
+of the target projection space.
 
 **Sparse** – When a sparse matrix is used for the random projection to reduce the computational complexity, then this 
-is a sparse projection. This is an alternate approach to Gaussian random projection matrix and 
-ensures the similar distance preserving quality while reducing the dimension.
-
-If we define $s = 1 / density$, the elements of the random matrix are drawn from
-
-\begin{equation}
-\begin{split}\left\{
-\begin{array}{c c l}
--\sqrt{\frac{s}{n_{\text{components}}}} & & 1 / 2s\\
-0 &\text{with probability}  & 1 - 1 / s \\
-+\sqrt{\frac{s}{n_{\text{components}}}} & & 1 / 2s\\
-\end{array}
-\right.\end{split}
-\end{equation}
+is a sparse projection. This is an alternate approach to Gaussian random projection matrix which ensures that a similar distance
+between the observations is preserved while reducing the dimensions.
 
 If the sparse projection matrix has $c$ nonzero entries per column, then the complexity of the operation 
 is of order $O(ckN)$ instead of $O(dkN)$.
 
-Now we can apply our random projection onto our dataset, which we loaded earlier. Once that function is done
-you can head over to the next function which is called `plot`. That function will plot the baseline and your
-chosen metric over the different dimensions.""")
+Now we can perform the same actions for our second dataset.""")
         self.accuracies = []
         self.dims = np.int32(np.linspace(1, self.data.shape[1], int(self.data.shape[1]/1)))
         # Loop over the projection sizes, k
         for dim in self.dims:
             # Create random projection
-            sp = SparseRandomProjection(n_components = dim)
+            sp = SparseRandomProjection(n_components = dim, random_state = 5)
             X = sp.fit_transform(self.X_train)
 
             # Train classifier of your choice on the sparse random projection
@@ -289,14 +271,15 @@ chosen metric over the different dimensions.""")
                         text= text)
         self.baseline(model = model, text = text, **kwargs)
         self.apply_random_projection(model = model, text = text, **kwargs)
-#         clear_output()
-#         self.plot(title = title)
         
     
     def _jans_birthday(self):
         self.cost = 0
         self._printmd(
-            """Dear Jan,
+            """
+# Birthday Present from Group 10
+
+Dear Jan,
 
 Group 10 (that includes Skyler MacGowan, Sebastian Sydow, Debasmita Dutta and Yannik Suhre), wishes you all the best for your
 birthday! We hope you have/had a beautiful day despite these challenging times! As a small birthday present, we have programmed
